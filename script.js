26adbde78f7c6e9d95dcf0852a153a6ccf873f1d
aprilsrp <43024156+aprilsrp@users.noreply.github.com>
2021-04-27 23:05:31 +0700

    update w/o activity

---
+++ script.js
@@ -0,0 +1,55 @@
+function moreDetail() {
+    var x = document.getElementById("1detail");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function moreDetail2() {
+    var x = document.getElementById("2detail");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function moreDetail3() {
+    var x = document.getElementById("3detail");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function moreDetail4() {
+    var x = document.getElementById("4detail");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function moreDetail5() {
+    var x = document.getElementById("5detail");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function moreDetail6() {
+    var x = document.getElementById("6detail");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function toggle_visibility(id) {
+       var e = document.getElementById(id);
+       if(e.style.display == 'block')
+          e.style.display = 'none';
+       else
+          e.style.display = 'block';
+    }
