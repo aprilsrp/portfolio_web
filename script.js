0c108e5fb198c6146bb6452c2a3fce853b2b2a97
aprilsrp <43024156+aprilsrp@users.noreply.github.com>
2022-06-14 19:24:10 +0700

    update
    ++ add work sharp web
    ++ bg nav
    --   post work

--- script.js
+++ script.js
@@ -24,55 +24,55 @@ function changeHMB3() {
   document.getElementById("how-human-begin").style.backgroundImage = "url('./image/web/xd-hbg.jpg')";
 }
 
-$(document).ready(function(){
-  $(window).scroll(function(){
-  	var scroll = $(window).scrollTop();
-	  if (scroll > 300) {
-	    $(".mynavbar").css("background" , "blue");
-	  }
+// $(document).ready(function(){
+//   $(window).scroll(function(){
+//   	var scroll = $(window).scrollTop();
+// 	  if (scroll > 300) {
+// 	    $(".mynavbar").css("background" , "blue");
+// 	  }
 
-	  else{
-		  $(".mynavbar").css("background" , "#333");  	
-	  }
-  })
-})
+// 	  else{
+// 		  $(".mynavbar").css("background" , "#333");  	
+// 	  }
+//   })
+// })
 
 
-function showAll() {
-  var all = document.querySelectorAll("#Grap, #Web, #Video, #Model");
-  if (all.style.display === "none") {
-    all.style.display = "block";
-  } else {
-    all.style.display = "none";
-  }
-}
+// function showAll() {
+//   var all = document.querySelectorAll("#Grap, #Web, #Video, #Model");
+//   if (all.style.display === "none") {
+//     all.style.display = "block";
+//   } else {
+//     all.style.display = "none";
+//   }
+// }
 
-function showGrap() {
-  var grap = document.getElementById("grap");
-  if (grap.style.display === "none") {
-    grap.style.display = "block";
-  } else {
-    grap.style.display = "none";
-  }
-}
+// function showGrap() {
+//   var grap = document.getElementById("grap");
+//   if (grap.style.display === "none") {
+//     grap.style.display = "block";
+//   } else {
+//     grap.style.display = "none";
+//   }
+// }
 
-function showWeb() {
-  var web = document.getElementById("web");
-  if (web.style.display === "none") {
-    web.style.display = "block";
-  } else {
-    web.style.display = "none";
-  }
-}
+// function showWeb() {
+//   var web = document.getElementById("web");
+//   if (web.style.display === "none") {
+//     web.style.display = "block";
+//   } else {
+//     web.style.display = "none";
+//   }
+// }
 
-function showVideo() {
-  var video = document.getElementById("video");
-  if (video.style.display === "none") {
-    video.style.display = "block";
-  } else {
-    video.style.display = "none";
-  }
-}
+// function showVideo() {
+//   var video = document.getElementById("video");
+//   if (video.style.display === "none") {
+//     video.style.display = "block";
+//   } else {
+//     video.style.display = "none";
+//   }
+// }
 
 function showModel() {
   var model = document.getElementById("model");
@@ -83,3 +83,22 @@ function showModel() {
   }
 }
 
+// $(document).scroll(function() {
+//   var y = $(this).scrollTop();
+//   if (y > 1000) {
+//     $('.toTop').fadeIn();
+//   } else {
+//     $('.toTop').fadeOut();
+//   }
+// });
+
+// $(document).scroll(function() { 
+//   if ($(document).scrollTop() > 1000) { 
+//  //To show the div 
+//    $('.toTop').show(); 
+//   } 
+//   else { 
+//    $('.toTop').hide(); 
+//  //To hide the div 
+//   } 
+//  }); 
